66aca11b9d603e8ff9d57c176dcdd6a3b1681ca5
Krishnavyshak R <krishnavyshak.dev@gmail.com>
2023-02-17 22:24:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
     
 setup(
     name='storr',
-    version='0.1.1',
+    version='0.1.2',
     description='A painless way to store session data',
     long_description_content_type='text/markdown',
     long_description=long_description,
