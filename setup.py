c55f9ce7069c57e11f5d6f32e86f40990ad6bdd2
Krishnavyshak R <krishnavyshak.dev@gmail.com>
2023-02-17 18:16:35 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='storr',
+    version='0.1.0',
+    description='A painless way to store session data',
+    packages=['storr'],
+    install_requires=['cryptography'],
+    author='Krishnavyshak R',
+    author_email='krishnavyshak.dev@gmail.com',
+    url='https://github.com/KrishaVyshak/storr',
+)
